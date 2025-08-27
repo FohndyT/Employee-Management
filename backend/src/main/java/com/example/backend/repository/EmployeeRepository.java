31873abad1a96406db28de224147a062509fa1fd
FohndyT <fohndytah@gmail.com>
2025-08-26 23:02:25 -0400

    Structure refactoring, repository and controller added

--- backend/src/main/java/com/example/backend/repository/EmployeeRepository.java
+++ backend/src/main/java/com/example/backend/repository/EmployeeRepository.java
@@ -1,6 +1,6 @@
-package repository;
+package com.example.backend.repository;
 
-import model.Employee;
+import com.example.backend.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
